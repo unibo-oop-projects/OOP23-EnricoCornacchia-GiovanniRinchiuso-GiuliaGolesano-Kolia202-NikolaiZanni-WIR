65a9b01440aa122321879977ce43528dd96a54e9
Nikolai Zanni <kolia.zanni@gmail,com>
2024-04-04 17:57:28 +0200

    Changes MainMenu with resume,quit,home buttom

--- src/main/java/it/unibo/view/impl/MainMenu.java
+++ src/main/java/it/unibo/view/impl/MainMenu.java
@@ -1,9 +1,9 @@
 package it.unibo.view.impl;
 
+import it.unibo.utilities.GameState;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
@@ -12,11 +12,11 @@ public class MainMenu extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        //Image config = new Image("config.png");'
-        //ImageView configView = new ImageView(config);
         final Button configButton = new Button("CONFIG");
         configButton.setFont(new Font(100));
         configButton.setOnMouseClicked(mouseEvent -> {
+            GameState.setGameState(GameState.PAUSED);
+            System.out.println("GameState" + GameState.getGameState());
             new AnotherStage().show();
         });
         primaryStage.setScene(new Scene(configButton));
@@ -36,17 +36,36 @@ public class MainMenu extends Application {
 }
 
     private static class AnotherStage extends Stage {
-        private static final int WIDTH = 100;
-        private static final int HEIGHT = 100;
+        private static final int WIDTH = 500;
+        private static final int HEIGHT = 300;
 
         AnotherStage() {
             super();
             setTitle("new Stage created" + System.currentTimeMillis());
             final VBox pane = new VBox();
-            pane.getChildren().add(new Label("RESUME"));
-            pane.getChildren().add(new Label("QUIT"));
-            pane.getChildren().add(new Label("HOME"));
+            Button resumeButton = new Button("RESUME");
+            Button quitButton = new Button("QUIT");
+            Button homeButton = new Button("HOME");
+            resumeButton.setFont(new Font(20));
+            quitButton.setFont(new Font(20));
+            homeButton.setFont(new Font(20));
+            resumeButton.setOnMouseClicked(event -> {
+                GameState.setGameState(GameState.PLAYING);
+                System.out.println(("GameState" + GameState.getGameState()));
+            });
+            quitButton.setOnMouseClicked(event -> {
+                GameState.setGameState(GameState.GAMEOVER);
+                System.out.println("GameState" + GameState.getGameState());
+            });
+            homeButton.setOnMouseClicked(event -> {
+                GameState.setGameState(GameState.HOME);
+                System.out.println("GameState" + GameState.getGameState());
+            });
+            pane.getChildren().add(resumeButton);
+            pane.getChildren().add(quitButton);
+            pane.getChildren().add(homeButton);
             setScene(new Scene(pane, WIDTH, HEIGHT));
+            show();
         }
     }
 }
