304256d53a3a0364bded5825b4cc84687021ca63
Nikolai Zanni <kolia.zanni@gmail.com>
2024-04-05 14:22:32 +0200

    changes HomeMenu

--- src/main/java/it/unibo/view/impl/HomeMenu.java
+++ src/main/java/it/unibo/view/impl/HomeMenu.java
@@ -1,1 +1,14 @@
 package it.unibo.view.impl;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class HomeMenu extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'start'");
+    }
+    
+}
