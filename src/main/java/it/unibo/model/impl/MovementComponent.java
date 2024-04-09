beeb738cf075e8d5b17aad70b93a3fb10e57520e
Giovanni Rinchiuso <giovanni.rinchiuso@studio.unibo.it>
2024-04-09 19:02:27 +0200

    Added and implemented MovementComponent

---
+++ src/main/java/it/unibo/model/impl/MovementComponent.java
@@ -0,0 +1,23 @@
+package it.unibo.model.impl;
+
+import it.unibo.common.Pair;
+import it.unibo.model.api.Entity;
+
+public class MovementComponent {
+
+    public void move(double x, double y, Entity E) {
+        double newX = E.getPosition().getX() + x;
+        double newY = E.getPosition().getY() + y;
+        if(canMove(x, y, E)){
+            E.setPosition(new Pair<>(newX, newY));
+        }
+    }
+
+
+    // al posto di 10 e 0 ci dovrebbero essere delle costanti, che però non sono state ancora definite
+    public boolean canMove(double x, double y, Entity E) {
+        double newX = E.getPosition().getX() + x;
+        double newY = E.getPosition().getY() + y;
+        return newX >= 0 && newX <= 10 && newY >= 0 && newY <= 10;
+    }
+}
