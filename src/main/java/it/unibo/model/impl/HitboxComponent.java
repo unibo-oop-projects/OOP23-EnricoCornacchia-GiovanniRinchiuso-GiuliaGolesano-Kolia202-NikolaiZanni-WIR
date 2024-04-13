acb2638344aae648492e0a9798e4ac4281f70727
Enrico Cornacchia <enrico.cornacchia@studio.unibo.it>
2024-04-13 17:17:16 +0200

    created several component Classes, to be completed

---
+++ src/main/java/it/unibo/model/impl/HitboxComponent.java
@@ -0,0 +1,18 @@
+package it.unibo.model.impl;
+
+import it.unibo.model.api.Component;
+import it.unibo.model.api.ComponentType;
+
+/**
+ * HitboxComponent
+ */
+public class HitboxComponent implements Component{
+
+    @Override
+    public ComponentType getComponent() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getComponent'");
+    }
+
+    
+}
---
+++ src/main/java/it/unibo/model/impl/LivesComponent.java
@@ -0,0 +1,18 @@
+package it.unibo.model.impl;
+
+import it.unibo.model.api.Component;
+import it.unibo.model.api.ComponentType;
+
+/**
+ * LivesComponent
+ */
+public class LivesComponent implements Component{
+
+    @Override
+    public ComponentType getComponent() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getComponent'");
+    }
+
+    
+}
