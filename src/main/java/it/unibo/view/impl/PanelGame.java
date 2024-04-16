a12bd2fa211ec395d31356940ed52e055accf5ad
kolia202 <kolia.zanni@gmail.com>
2024-04-16 14:35:41 +0200

    corrrection pmd error

--- src/main/java/it/unibo/view/impl/PanelGame.java
+++ src/main/java/it/unibo/view/impl/PanelGame.java
@@ -4,18 +4,22 @@ import java.awt.Color;
 import java.awt.Dimension;
 
 import javax.swing.JPanel;
+
 /**
  * PanelGame, it represents the game panel.
  */
 public class PanelGame extends JPanel {
 
-    //game window dimensionsk
+    private static final long serialVersionUID = 1L;
+
+    // game window dimensionsk
     private static final int TILE_PIXELS = 16;
     private static final int SCALE = 3;
     private static final int COL = 12;
     private static final int ROW = 16;
     private final int height = TILE_PIXELS * SCALE * ROW;
     private final int width = TILE_PIXELS * SCALE * COL;
+
     /**
      * Constructor of the class.
      */
