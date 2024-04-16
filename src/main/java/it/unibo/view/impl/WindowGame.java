752f30a79cd40bca942692729ccf39b702780516
Giovanni Rinchiuso <giovanni.rinchiuso@studio.unibo.it>
2024-04-16 14:34:58 +0200

    Corrected pmd error about exception in constructor in window game

--- src/main/java/it/unibo/view/impl/WindowGame.java
+++ src/main/java/it/unibo/view/impl/WindowGame.java
@@ -11,9 +11,8 @@ public final class WindowGame {
      * main, it starts the game window.
      * 
      * @param args the arguments of the main.
-     * @throws Exception if an error occurs.
      */
-    public static void main(final String[] args) throws Exception {
+    public static void main(final String[] args) {
         final JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
