13fd2b244cde007fa990f8ccb93826baaa628cf8
Enrico Cornacchia <enrico.cornacchia@studio.unibo.it>
2024-04-20 19:32:53 +0200

    fixed Rectangle class checkstyle

--- src/main/java/it/unibo/common/Rectangle.java
+++ src/main/java/it/unibo/common/Rectangle.java
@@ -1,51 +1,91 @@
 package it.unibo.common;
 
+/**
+ * Represents a rectangle in a 2D coordinate system.
+ */
 public class Rectangle {
     private double x;
     private double y;
     private final double width;
     private final double height;
 
-    public Rectangle(double x, double y, double width, double height) {
+    /**
+     * Constructs a new Rectangle with the specified position and dimensions.
+     *
+     * @param x      the x-coordinate of the rectangle
+     * @param y      the y-coordinate of the rectangle
+     * @param width  the width of the rectangle
+     * @param height the height of the rectangle
+     */
+    public Rectangle(final double x, final double y, final double width, final double height) {
+        this.x = x;
+        this.y = y;
         this.width = width;
         this.height = height;
     }
 
+    /**
+     * Returns the x-coordinate of the rectangle.
+     *
+     * @return the x-coordinate.
+     */
     public double getX() {
         return x;
     }
 
-    public void setX(double x) {
+    /**
+     * Sets the x-coordinate of the rectangle.
+     *
+     * @param x the new x-coordinate.
+     */
+    public void setX(final double x) {
         this.x = x;
     }
 
+    /**
+     * Returns the y-coordinate of the rectangle.
+     *
+     * @return the y-coordinate.
+     */
     public double getY() {
         return y;
     }
 
-    public void setY(double y) {
+    /**
+     * Sets the y-coordinate of the rectangle.
+     *
+     * @param y the new y-coordinate.
+     */
+    public void setY(final double y) {
         this.y = y;
     }
 
+    /**
+     * Returns the width of the rectangle.
+     *
+     * @return the width of the rectangle
+     */
     public double getWidth() {
         return width;
     }
 
+    /**
+     * Returns the height of the rectangle.
+     *
+     * @return the height of the rectangle
+     */
     public double getHeight() {
         return height;
     }
 
-    public double getArea() {
-        return width * height;
-    }
-
-    public double getPerimeter() {
-        return 2 * (width + height);
-    }
-
-    public boolean intersects(Rectangle other) {
-        // This is a simple intersection check assuming rectangles are axis-aligned and origin is at the top-left
-        // You may need to adjust this depending on your coordinate system and rectangle representation
-        return this.width > other.width && this.height > other.height;
+    /**
+     * Checks if this rectangle intersects with another rectangle.
+     *
+     * @param other the other rectangle to check for intersection
+     * @return true if the rectangles intersect, false otherwise
+     */
+    public boolean intersects(final Rectangle other) {
+        return this.getX() < other.getX() + other.getWidth() && this.getX() + this.getWidth() > other.getX() 
+               && this.getY() < other.getY() + other.getHeight() && this.getY() + this.getHeight() > other.getY();
     }
 }
