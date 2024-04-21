3a08d812d54721f468d0324b547decea58a92326
kolia202 <kolia.zanni@gmail.com>
2024-04-21 16:19:27 +0200

    add method LivesComponent and PointsComponent

--- src/main/java/it/unibo/model/impl/LivesComponent.java
+++ src/main/java/it/unibo/model/impl/LivesComponent.java
@@ -7,14 +7,40 @@ import it.unibo.model.api.ComponentType;
  * LivesComponent, it represents the lives of the entity.
  */
 public class LivesComponent implements Component {
+
+    private int lives;
+
+    /*
+     * Initialize lives to 3.
+     */
+    public LivesComponent() {
+        this.lives = 3;
+    }
+
+    /**
+     * Method of stealing a life.
+     */
+    public void stealLives() {
+        this.lives = this.lives - 1;
+    }
+    
+
+    /**
+     * Method to get the current lives.
+     * 
+     * @return the current numer of lives.
+     */
+    public final int getLives() {
+        return this.lives;
+    }
+
     /**
      * getter of the type of the class.
+     * 
      * @return the type of the class.
      */
     @Override
     public ComponentType getComponent() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getComponent'");
+        return ComponentType.LIFE;
     }
-
 }
--- src/main/java/it/unibo/model/impl/PointsComponent.java
+++ src/main/java/it/unibo/model/impl/PointsComponent.java
@@ -7,24 +7,34 @@ import it.unibo.model.api.ComponentType;
  * PointsComponent, it represents the points acquired by an entity.
  */
 public class PointsComponent implements Component {
-    
+
     private int points;
+
     /*
      * Initialize points to 0.
      */
     public PointsComponent() {
         this.points = 0;
     }
+
     /**
      * Method to add ponits.
      */
     public void addPoints(final int pointsToAdd) {
         this.points += pointsToAdd;
     }
+
     /**
-     * Method to get the total points acquired.
+     * Method of stealing points
      */
-    public int getTotalPoints() {
+    public void stealPoint(final int pointsToSteal) {
+        this.points -= pointsToSteal;
+    }
+
+    /**
+     * Method to get the current points.
+     */
+    public int getPoints() {
         return this.points;
     }
 
