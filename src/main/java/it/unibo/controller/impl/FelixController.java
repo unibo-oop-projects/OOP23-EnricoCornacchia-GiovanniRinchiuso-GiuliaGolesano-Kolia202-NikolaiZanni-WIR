137c57803e9c1b5e4ec33f630423674ff9241b45
Enrico Cornacchia <enrico.cornacchia@studio.unibo.it>
2024-04-21 19:22:45 +0200

    implemented some movements methods of the FelixController

--- src/main/java/it/unibo/controller/impl/FelixController.java
+++ src/main/java/it/unibo/controller/impl/FelixController.java
@@ -1,37 +1,54 @@
 package it.unibo.controller.impl;
 
+import it.unibo.model.api.ComponentType;
+import it.unibo.model.api.Entity;
+import it.unibo.model.impl.EntityFactoryImpl;
+import it.unibo.model.impl.MovementComponent;
+import it.unibo.model.impl.LivesComponent;
+import it.unibo.utilities.Constaints.Felix;
+
 /**
  * Controller for the Felix character.
  */
 public class FelixController {
 
+    private final EntityFactoryImpl entityFactoryImpl = new EntityFactoryImpl();
+    private Entity felix;
+
+    /**
+     * Constructs a new FelixController object.
+     * Initializes the felix instance using the provided entityFactoryImpl.
+     */
+    public FelixController() {
+        this.felix = entityFactoryImpl.createFelix(Felix.FELIX_START);
+    }
     /**
      * Move the character to the right.
      */
     public void moveRight() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'moveRight'");
+        MovementComponent moveComp = (MovementComponent) this.felix.getComponent(ComponentType.MOVEMENT).get();
+        moveComp.move(1, 0, this.felix);
     }
     /**
      * Move the character to the left.
      */
     public void moveLeft() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'moveLeft'");
+        MovementComponent moveComp = (MovementComponent) this.felix.getComponent(ComponentType.MOVEMENT).get();
+        moveComp.move(-1, 0, this.felix);
     }
     /**
      * Move the character down.
      */
     public void moveDown() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'moveDown'");
+        MovementComponent moveComp = (MovementComponent) this.felix.getComponent(ComponentType.MOVEMENT).get();
+        moveComp.move(0, -1, this.felix);
     }
     /**
      * Move the character up.
      */
     public void moveUp() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'moveUp'");
+        MovementComponent moveComp = (MovementComponent) this.felix.getComponent(ComponentType.MOVEMENT).get();
+        moveComp.move(0, 1, this.felix);
     }
     /**
      * Jump.
@@ -45,7 +62,7 @@ public class FelixController {
      * @return true if Felix have more than 0 lives, false otherwise.
      */
     public boolean isAlive() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isAlive'");
+        LivesComponent lives = (LivesComponent) this.felix.getComponent(ComponentType.LIFE).get();
+        return lives.getLives() > 0;
     }
 }
