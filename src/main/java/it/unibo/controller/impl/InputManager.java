1091b0588cae7227ef8d233c2b3500cd2854a6fb
Giulia Golesano <giulia.golesano@studio.unibo.it>
2024-04-22 12:27:01 +0200

    added useful methods for new classes

--- src/main/java/it/unibo/controller/impl/InputManager.java
+++ src/main/java/it/unibo/controller/impl/InputManager.java
@@ -1,14 +1,34 @@
 package it.unibo.controller.impl;
 /**
  * Controller for the input.
- 
  */
+
+import it.unibo.model.api.GamePerformance;
+
 public class InputManager {
-    /**
-     * Get the input from the user.
-     * @return the input from the user
-     */
-    public int getInput() {
-        return 0; 
+
+    private final GamePerformance gamePerformance;
+
+    public InputManager(final GamePerformance gamePerformance){
+        this.gamePerformance = gamePerformance;
     }
+
+    /**
+     * Method that notify the Game Performance that a key from keyboard has been pressed.
+     * 
+     * @param KeyCode
+     */
+    public void notifyKeyPressed(final int KeyCode){
+        this.gamePerformance.addKey(KeyCode);
+    }
+
+    /**
+     * Method that notify the Game Performance that a key from keyboard has been released.
+     * 
+     * @param KeyCode
+     */
+    public void notifyKeyReleased(final int KeyCode){
+        this.gamePerformance.removeKey(KeyCode);
+    }
+
 } 
