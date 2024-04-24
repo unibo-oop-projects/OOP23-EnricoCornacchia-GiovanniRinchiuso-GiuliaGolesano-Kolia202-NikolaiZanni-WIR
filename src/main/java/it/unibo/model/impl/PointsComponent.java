41146b3956ba611345d80a91a36387bc5ce9eda3
kolia202 <kolia.zanni@gmail.com>
2024-04-24 12:55:16 +0200

    Change PointsComponent

--- src/main/java/it/unibo/model/impl/PointsComponent.java
+++ src/main/java/it/unibo/model/impl/PointsComponent.java
@@ -2,6 +2,7 @@ package it.unibo.model.impl;
 
 import it.unibo.model.api.Component;
 import it.unibo.model.api.ComponentType;
+import it.unibo.view.impl.PointsView;
 
 /**
  * PointsComponent, it represents the points acquired by an entity.
@@ -9,26 +10,37 @@ import it.unibo.model.api.ComponentType;
 public class PointsComponent implements Component {
 
     private int points;
+    private PointsView pointsView;
 
     /*
      * Initialize points to 0.
      */
     public PointsComponent() {
-        this.points = 0;
+        this.points = 10000;
     }
 
     /**
-     * Method to add ponits.
+     * Method to add points.
      */
     public void addPoints(final int pointsToAdd) {
         this.points += pointsToAdd;
+        if (pointsView != null) {
+            pointsView.updatePointsLabel();
+        }
     }
-    
+
     /**
      * Method to get the current points.
      */
     public int getPoints() {
         return this.points;
+    }
+
+    /**
+     * Method to set the PointsView.
+     */
+    public void setPointsView(PointsView pointsView) {
+        this.pointsView = pointsView;
     }
 
     /**
@@ -40,6 +52,7 @@ public class PointsComponent implements Component {
     public ComponentType getComponent() {
         return ComponentType.POINTS;
     }
+
     /**
      * {@inheritDoc}.
      */
--- src/main/java/it/unibo/view/impl/PointsView.java
+++ src/main/java/it/unibo/view/impl/PointsView.java
@@ -3,29 +3,33 @@ package it.unibo.view.impl;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import it.unibo.model.impl.PointsComponent;
+import it.unibo.utilities.Constaints;
 
 public class PointsView extends StackPane {
-
     private PointsComponent pointsComponent;
 
     public PointsView(PointsComponent pointsComponent) {
         this.pointsComponent = pointsComponent;
+        this.pointsComponent.setPointsView(this);
         updatePointsLabel();
     }
 
-    private void updatePointsLabel() {
-        ImageView pointsBackground = new ImageView("");
-        pointsBackground.setFitWidth(100);
-        pointsBackground.setFitHeight(100);
+    public void updatePointsLabel() {
+        ImageView pointsBackground = new ImageView("PointsTable.png");
+        pointsBackground.setFitWidth(225);
+        pointsBackground.setFitHeight(Constaints.HEIGHT_PAUSE_BUTTON);
 
-        Label pointsLabel = new Label("Points: " + pointsComponent.getPoints());
-        pointsLabel.setStyle("-fx-font-size: 24; -fx-font-weight: bold; -fx-text-fill: white; -fx-background-color: transparent;");
+        Label pointsLabel = new Label("score: " + pointsComponent.getPoints());
+        pointsLabel.setStyle("-fx-font-size: 18; -fx-font-weight: bold; -fx-text-fill: white; -fx-background-color: transparent;");
         pointsLabel.setPrefSize(pointsBackground.getFitWidth(), pointsBackground.getFitHeight());
         pointsLabel.setAlignment(Pos.CENTER);
+        AnchorPane.setLeftAnchor(pointsLabel, 10.0);
+        AnchorPane.setBottomAnchor(pointsLabel, 25.0);
 
-        StackPane pointsContainer = new StackPane(pointsBackground, pointsLabel);
+        AnchorPane pointsContainer = new AnchorPane(pointsBackground, pointsLabel);
         getChildren().add(pointsContainer);
     }
 }
