a0443758e3f8e99d2382609c81440c7b9669a24d
Giulia Golesano <giulia.golesano@studio.unibo.it>
2024-04-27 20:15:27 +0200

    added some variables constructors in Application Implementation

--- src/main/java/it/unibo/controller/impl/ApplicationImpl.java
+++ src/main/java/it/unibo/controller/impl/ApplicationImpl.java
@@ -1,6 +1,10 @@
 package it.unibo.controller.impl;
 
 import it.unibo.controller.api.Application;
+import it.unibo.core.api.GameEngine;
+import it.unibo.core.impl.GameEngineImpl;
+import it.unibo.model.api.GamePerformance;
+import it.unibo.model.impl.GamePerformanceImpl;
 
 public class ApplicationImpl implements Application {
 
@@ -10,17 +14,39 @@ public class ApplicationImpl implements Application {
     private GameController gameController;
     private InputManager inputManager;
     private RalphController ralphController;
+    private GamePerformance gamePerformance;
+    private GameEngine gameEngine;
 
     /**
      * Constructor.
      */
     public ApplicationImpl() {
+        setController();
+        this.gamePerformance = new GamePerformanceImpl(this.gameController);
+        this.gameEngine = new GameEngineImpl();
+        this.gameEngine.mainLoop();
+    }
+
+    private void setController(){
         this.brickController = new BrickController();
         this.collisionManager = new CollisionManager();
         this.felixController = new FelixController();
         this.gameController = new GameController();
-        this.inputManager = new InputManager();
         this.ralphController = new RalphController();
+    }
+
+    /**
+     * Method to update.
+     */
+    public void update(){
+        this.gameController.update();
+    }
+
+    /**
+     * Redraw method.
+     */
+    public void drawUpdate(){
+
     }
 
     /**
