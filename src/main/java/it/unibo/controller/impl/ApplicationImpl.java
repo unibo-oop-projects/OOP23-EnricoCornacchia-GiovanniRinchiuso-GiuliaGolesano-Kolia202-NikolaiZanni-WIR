48b9f7a8b8ced61b83730da258a9a0343cc23863
Enrico Cornacchia <enrico.cornacchia@studio.unibo.it>
2024-05-01 16:16:34 +0200

    Implemented the CollisionManager

--- src/main/java/it/unibo/controller/impl/ApplicationImpl.java
+++ src/main/java/it/unibo/controller/impl/ApplicationImpl.java
@@ -30,7 +30,7 @@ public class ApplicationImpl implements Application {
     }
     private void setController() {
         this.brickController = new BrickController(gamePerformance);
-        this.collisionManager = new CollisionManager();
+        this.collisionManager = new CollisionManager(gamePerformance.getEntity());
         this.felixController = new FelixController();
         this.gameController = new GameController();
         this.ralphController = new RalphController(0);
--- src/main/java/it/unibo/controller/impl/CollisionManager.java
+++ src/main/java/it/unibo/controller/impl/CollisionManager.java
@@ -1,14 +1,52 @@
 package it.unibo.controller.impl;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import it.unibo.model.api.Component;
+import it.unibo.model.api.ComponentType;
+import it.unibo.model.api.Entity;
+
 /**
  * Controller for the collision.
  */
 public class CollisionManager {
+
+    private Set<Component> hitboxes;
+
+    /**
+     * Constructor for the CollisionManager.
+     * @param entities the set of entities.
+     */
+    public CollisionManager(final Set<Entity> entities) {
+        this.hitboxes = new HashSet<>();
+        hitboxes.addAll(entities.stream()
+            .flatMap(e -> e.getComponent(ComponentType.HITBOX).stream())
+            .collect(Collectors.toSet()));
+    }
+
+    /**
+     * Getter for the hitboxes.
+     * @return the set of hitboxes.
+     */
+    public Set<Component> getHitboxes() {
+        return this.hitboxes;
+    }
+
+    /**
+     * Add a hitbox to the set of hitboxes.
+     * @param hitbox the hitbox to add.
+     */
+    public void addHitbox(final Component hitbox) {
+        this.hitboxes.add(hitbox);
+    }
+
     /**
      * Check if there is a collision.
      */
     public void check() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'check'");
+        this.hitboxes.forEach(h -> h.update());
     }
 
 }
