9ab43b33ccc383840eb29402ec1696827d1e5e6f
Enrico Cornacchia <enrico.cornacchia@studio.unibo.it>
2024-05-16 18:07:49 +0200

    added RalphView class for handling the view of Ralph

---
+++ src/main/java/it/unibo/view/impl/RalphView.java
@@ -0,0 +1,23 @@
+package it.unibo.view.impl;
+
+import it.unibo.view.api.View;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+
+/**
+ * Class responsible for the view of Ralph.
+ */
+public class RalphView implements View{
+
+    @Override
+    public void draw(GraphicsContext g) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'draw'");
+    }
+
+    @Override
+    public Image getSource(String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getSource'");
+    }
+}
