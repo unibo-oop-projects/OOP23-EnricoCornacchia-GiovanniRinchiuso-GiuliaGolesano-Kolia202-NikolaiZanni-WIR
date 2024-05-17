69d09b13178bfb2d0a17ec12ebc2c65727f1a06e
Giulia Golesano <giulia.golesano@studio.unibo.it>
2024-05-17 18:01:23 +0200

    create a window view to manage the animation

---
+++ src/main/java/it/unibo/view/impl/WindowsView.java
@@ -0,0 +1,21 @@
+package it.unibo.view.impl;
+
+import it.unibo.view.api.View;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+
+public class WindowsView implements View{
+
+    @Override
+    public void draw(GraphicsContext g) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'draw'");
+    }
+
+    @Override
+    public Image getSource(String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getSource'");
+    }
+    
+}
