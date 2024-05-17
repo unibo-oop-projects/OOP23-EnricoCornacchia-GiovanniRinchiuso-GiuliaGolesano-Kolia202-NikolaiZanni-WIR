320f0b3a5cf14f19a204353d982aa12a14216318
Giulia Golesano <giulia.golesano@studio.unibo.it>
2024-05-17 18:01:52 +0200

    create a class to manage the windows position

---
+++ src/main/java/it/unibo/common/WindowPosition.java
@@ -0,0 +1,22 @@
+package it.unibo.common;
+/**
+ * Class to manage a window position.
+ */
+public class WindowPosition {
+    private int x;
+    private int y;
+
+    public WindowPosition(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
+
