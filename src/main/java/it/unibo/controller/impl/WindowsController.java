792b287b6c4b581699d206d549e6481fc18f94d6
Giulia Golesano <giulia.golesano@studio.unibo.it>
2024-05-17 17:56:01 +0200

    added method checking if there are still broken windows

--- src/main/java/it/unibo/controller/impl/WindowsController.java
+++ src/main/java/it/unibo/controller/impl/WindowsController.java
@@ -1,8 +1,10 @@
 package it.unibo.controller.impl;
 
+import it.unibo.model.api.Component;
 import it.unibo.model.api.ComponentType;
 import it.unibo.model.api.GamePerformance;
 import it.unibo.model.impl.EntityFactoryImpl;
+import it.unibo.model.impl.FixedWindowsComponent;
 
 public class WindowsController {
     private final EntityFactoryImpl entityFactoryImpl;
@@ -14,35 +16,15 @@ public class WindowsController {
     public WindowsController(final GamePerformance gamePerformance) {
         this.gamePerformance = gamePerformance;
         this.entityFactoryImpl = new EntityFactoryImpl(this.gamePerformance);
-        switch (this.gamePerformance.getLevel()) {
-            case 1: 
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            default: 
-                break;
-        }
-    }
-    /**
-     * Method that create a floor of windows.
-     * @param y the y coordinate of the floor.
-     */
-    private void createLine(final double y) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'createLine'");
     }
     /**
      * Method that check if there are broken windows.
      * @return true if the game is won, false otherwise.
      */
     public boolean isWon() {
-        for(var w: this.gamePerformance.getWindows()){
-            
-        }
-        return false;
+        return this.gamePerformance.getWindows().stream()
+            .allMatch(w -> w.getTheComponent(ComponentType.FIXEDWINDOWS)
+                            .map(c -> ((FixedWindowsComponent) c).getFixed())
+                            .orElse(false));
     }
 }
