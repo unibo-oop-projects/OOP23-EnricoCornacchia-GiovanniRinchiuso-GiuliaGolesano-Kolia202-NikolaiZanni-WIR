9dd44bc81e69710756b92397f53f5de40441bb86
Giulia Golesano <giulia.golesano@studio.unibo.it>
2024-05-19 14:46:22 +0200

    create the view of a window with the creation of the animation

--- src/main/java/it/unibo/view/impl/WindowsView.java
+++ src/main/java/it/unibo/view/impl/WindowsView.java
@@ -1,21 +1,72 @@
 package it.unibo.view.impl;
 
 import it.unibo.view.api.View;
-import javafx.scene.canvas.GraphicsContext;
+import javafx.animation.Timeline;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
+import javafx.util.Duration;
 
 public class WindowsView implements View{
-
-    @Override
-    public void draw(GraphicsContext g) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'draw'");
+    private static final int FRAME_COUNT = 4;
+    private static final int FRAME_WIDTH = 128; 
+    private static final int FRAME_HEIGHT = 128; 
+    private static final int ANIMATION_DURATION = 1000; 
+    private ImageView imageView;
+    private Image spriteSheet;
+    private Timeline timeline;
+    private int currentFrame;
+    /**
+     * Constructor.
+     */
+    public WindowsView() {
+        spriteSheet = getSource("window");
+        imageView = new ImageView();
+        imageView.setFitHeight(FRAME_HEIGHT);
+        imageView.setFitWidth(FRAME_WIDTH);        
     }
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public Image getSource(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getSource'");
+    public Image getSource(final String name) {
+        return new Image("/src/main/resources/" + name + ".png");
     }
-    
+    /**
+     * Method to change the animation of the fixing window.
+     * @return
+     */
+    public void fixAnimation() {
+        if (timeline != null && timeline.getStatus() == Animation.Status.RUNNING) return ;
+
+        currentFrame = 0;
+        timeline = new Timeline(new KeyFrame(Duration.millis(ANIMATION_DURATION / FRAME_COUNT), e -> updateFrame()));
+        timeline.setCycleCount(FRAME_COUNT);
+        timeline.setOnFinished(e -> imageView.setImage(getFrame(FRAME_COUNT - 1)));
+        timeline.play();
+    }
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void updateFrame() {
+        imageView.setImage(getFrame(currentFrame));
+        currentFrame++;
+    }
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Image getFrame(int index) {
+        return new WritableImage(spriteSheet.getPixelReader(), index * FRAME_WIDTH, 0, FRAME_WIDTH, FRAME_HEIGHT);
+    }
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public ImageView getImageView() {
+        return this.imageView;
+    }
 }
