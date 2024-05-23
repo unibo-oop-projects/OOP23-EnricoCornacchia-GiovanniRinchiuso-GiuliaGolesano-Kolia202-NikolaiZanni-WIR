744357c5029bfa3928fb433526d8ee76496492f7
Giulia Golesano <giulia.golesano@studio.unibo.it>
2024-05-23 14:54:36 +0200

    fixed windows component added

--- src/test/java/it/unibo/model/FixedWindowsComponentTest.java
+++ src/test/java/it/unibo/model/FixedWindowsComponentTest.java
@@ -1,5 +1,30 @@
 package it.unibo.model;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import it.unibo.model.api.ComponentType;
+import it.unibo.model.impl.FixedWindowsComponent;
+/**
+ * Class to test the fixed window component.
+ */
 public class FixedWindowsComponentTest {
-    
+
+    @Test
+    public void testSetFixed() {
+        FixedWindowsComponent fixedWindowsComponent = new FixedWindowsComponent(false);
+        fixedWindowsComponent.setFixed();
+        assertEquals(true, fixedWindowsComponent.getFixed());
+    }
+
+    @Test
+    public void testGetFixed() {
+        FixedWindowsComponent fixedWindowsComponent = new FixedWindowsComponent(true);
+        assertEquals(true, fixedWindowsComponent.getFixed());
+    }
+
+    @Test
+    public void testGetComponent() {
+        FixedWindowsComponent fixedWindowsComponent = new FixedWindowsComponent(true);
+        assertEquals(ComponentType.FIXEDWINDOWS, fixedWindowsComponent.getComponent());
+    }
 }
