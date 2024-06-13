a3324c7a35ff61059f245c84474156518210f0d3
Enrico Cornacchia <enrico.cornacchia@studio.unibo.it>
2024-06-13 18:34:11 +0200

    checkstyle ImmortalityComponent and StopRalphComponent

--- src/main/java/it/unibo/model/impl/ImmortalityComponent.java
+++ src/main/java/it/unibo/model/impl/ImmortalityComponent.java
@@ -1,21 +1,25 @@
 package it.unibo.model.impl;
 
 import it.unibo.model.api.ComponentType;
-
+/**
+ * Component to manage the immortality of the entity.
+ */
 public class ImmortalityComponent extends AbstractComponent {
-    final int STARTTIME = 10000;
+    private static final int STARTTIME = 10000;
     private long startTime;
-    /*
+    /**
      * Method to set the immortality.
+     * @param livesComponent the lives component to set the immortality.
      */
-    public void setImmortality(LivesComponent livesComponent) {
+    public void setImmortality(final LivesComponent livesComponent) {
         livesComponent.setImmortality();
         this.startTime = System.currentTimeMillis(); 
     }
-    /*
+    /**
      * Method to check if the immortality is stopped.
+     * @param livesComponent the lives component to check.
      */
-    public void chekStopImmortality(LivesComponent livesComponent) {
+    public void chekStopImmortality(final LivesComponent livesComponent) {
         if (livesComponent.isImmortality()) {
             long currentTime = System.currentTimeMillis();
             if (currentTime - this.startTime >= STARTTIME) { 
@@ -23,6 +27,11 @@ public class ImmortalityComponent extends AbstractComponent {
             }
         }
     }
+    /**
+     * Returns the component type of this ImmortalityComponent.
+     *
+     * @return the component type of this ImmortalityComponent
+     */
     @Override
     public ComponentType getComponent() {
         return ComponentType.IMMORTALITY;
