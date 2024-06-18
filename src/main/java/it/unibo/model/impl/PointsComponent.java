5bc146acd6c1494fb5403e11c2808501513a9a1a
Giulia Golesano <giulia.golesano@studio.unibo.it>
2024-06-18 19:33:23 +0200

    checkstyle

--- src/main/java/it/unibo/model/impl/PointsComponent.java
+++ src/main/java/it/unibo/model/impl/PointsComponent.java
@@ -2,7 +2,6 @@ package it.unibo.model.impl;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
