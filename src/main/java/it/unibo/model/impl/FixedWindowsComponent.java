2f0b75130836ca12ce85cc16e0a037976ffcafd9
Giovanni Rinchiuso <giovanni.rinchiuso@studio.unibo.it>
2024-06-23 10:51:32 +0200

    Removed Unuseful Comments

--- src/main/java/it/unibo/model/impl/FixedWindowsComponent.java
+++ src/main/java/it/unibo/model/impl/FixedWindowsComponent.java
@@ -24,7 +24,6 @@ public class FixedWindowsComponent extends AbstractComponent {
      * Set the boolean variable.
      */
     public void setFixed() {
-        //System.err.println("Valore settato anche dentro al fixedwindows");
         this.isFixed = true;
     }
     /**
